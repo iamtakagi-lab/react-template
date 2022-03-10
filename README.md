64ffedee2cba851881922d00229ee820e99d224c
takagi <iam.takagi.dev@gmail.com>
2022-03-10 21:20:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-template
+
+## LICENSE
+MIT License.
---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const config: webpack.Configuration = {
+    mode: isProduction ? "production" : "development",
+    entry: {
+        main: "./src/index.tsx",
+    },
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.tsx?$/, /\.ts$/],
+                loader: "ts-loader",
+                options: { compilerOptions: { module: "ES2020", moduleResolution: "node" } },
+            }
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"],
+    }
+}
+
+module.exports = config
